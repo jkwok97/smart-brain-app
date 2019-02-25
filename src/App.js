8b140278032dca8de8accab170f8bc1e8d7fc1eb
Jeff Kwok <jkwok97@gmail.com>
2019-02-25 14:05:54 -0700

    completed profile to app

--- src/App.js
+++ src/App.js
@@ -35,7 +35,8 @@ const initialState = {
     name: '',
     email: '',
     entries: 0,
-    joined: ''
+    joined: '',
+    age: ''
   }
 }
 
@@ -57,7 +58,8 @@ class App extends Component {
         name: data.name,
         email: data.email,
         entries: data.entries,
-        joined: data.joined
+        joined: data.joined,
+        age: data.age
     }})
   }
 
@@ -131,7 +133,7 @@ class App extends Component {
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, boxes, isProfileOpen } = this.state;
+    const { isSignedIn, imageUrl, route, boxes, isProfileOpen, user } = this.state;
     return (
       <div className="App">
          <Particles className='particles'
@@ -144,7 +146,12 @@ class App extends Component {
         />
         { isProfileOpen &&
         <Modal>
-          <Profile isProfileOpen={isProfileOpen} toggleModal={this.toggleModal}/>
+          <Profile 
+            isProfileOpen={isProfileOpen} 
+            toggleModal={this.toggleModal} 
+            user={user}
+            loadUser={this.loadUser}
+          />
         </Modal>
         }
         { route === 'home'
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,35 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Profile.css';
 
-const Profile = ({ isProfileOpen, toggleModal }) => {
-    return (
-        <div className='profile-modal'>
-            <article className="br3 ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-gray">
-            <main className="pa4 white-80 w-80">
-                <img
-                    src="http://tachyons.io/img/logo.jpg"
-                    className="h3 w3 dib" alt="avatar" />
-                <h1>John Doe</h1>
-                <h4>Images Submitted: 5</h4>
-                <p>Member Since: January</p>
-                <hr />
-                <label className='mt2 fw6' htmlFor="user-name">Name:</label>
-                    <input className="pa2 ba w-100" placeholder="john" type="text" name="user-name"  id="name" />
-                <label className='mt2 fw6' htmlFor="user-age">Age:</label>
-                    <input className="pa2 ba w-100" placeholder="56" type="text" name="user-age"  id="age" />
-                <label className='mt2 fw6' htmlFor="user-email">Email:</label>
-                    <input className="pa2 ba w-100" placeholder="email" type="text" name="user-email"  id="email" />
-                <div className="mt4" style={{display: 'flex', justifyContent: 'space-evenly'}}>
-                    <button className="b pa2 grow pointer hover-white w-40 bg-dark-gray b--white-20 white">Save</button>
-                    <button className="b pa2 grow pointer hover-white w-40 bg-dark-gray b--white-20 white"
-                        onClick={toggleModal}>
-                        Cancel
-                    </button>
-                </div>  
-            </main>
-            <div className='modal-close' onClick={toggleModal}>&times;</div>
-            </article>
-        </div>
-    )}
-
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: this.props.user.name,
+            age: this.props.user.age,
+            email: this.props.user.email
+        }
+    }
+    onFormChange = (event) => {
+        switch(event.target.name) {
+            case 'user-name':
+                this.setState({name: event.target.value})
+                break;
+            case 'user-age':
+                this.setState({age: event.target.value})
+                break;
+            case 'user-email':
+                this.setState({email: event.target.value})
+                break;
+            default:
+                return;
+        }
+    }
+    onProfileUpdate = (data) => {
+        fetch(`http://localhost:3000/profile/${this.props.user.id}`, {
+            method: 'post',
+            headers: {'content-Type': 'application/json'},
+            body: JSON.stringify({ formInput: data })
+        }).then(resp => {
+            this.props.toggleModal();
+            this.props.loadUser({...this.props.user, ...data});
+        }).catch(console.log);
+    }
+    render() {
+        const { user } = this.props;
+        const { name, age, email } = this.state;
+        return (
+            <div className='profile-modal'>
+                <article className="br3 ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-gray">
+                <main className="pa4 white-80 w-80">
+                    <img
+                        src="http://tachyons.io/img/logo.jpg"
+                        className="h3 w3 dib" alt="avatar" />
+                    <h1>{this.state.name}</h1>
+                    <h4>{`Images Submitted: ${user.entries}`}</h4>
+                    <p>{`Member Since: ${new Date(user.joined).toLocaleDateString()}`}</p>
+                    <hr />
+                    <label className='mt2 fw6' htmlFor="user-name">Name:</label>
+                        <input onChange={this.onFormChange} className="pa2 ba w-100 black" placeholder={user.name} type="text" name="user-name"  id="name" />
+                    <label className='mt2 fw6' htmlFor="user-age">Age:</label>
+                        <input onChange={this.onFormChange} className="pa2 ba w-100 black" placeholder={user.age}type="text" name="user-age"  id="age" />
+                    <label className='mt2 fw6' htmlFor="user-email">Email:</label>
+                        <input onChange={this.onFormChange} className="pa2 ba w-100 black" placeholder={user.email} type="text" name="user-email"  id="email" />
+                    <div className="mt4" style={{display: 'flex', justifyContent: 'space-evenly'}}>
+                        <button className="b pa2 grow pointer hover-white w-40 bg-dark-gray b--white-20 white"
+                            onClick={()=>this.onProfileUpdate({ name, age, email })}>
+                            Save
+                        </button>
+                        <button className="b pa2 grow pointer hover-white w-40 bg-dark-gray b--white-20 white"
+                            onClick={this.props.toggleModal}>
+                            Cancel
+                        </button>
+                    </div>  
+                </main>
+                <div className='modal-close' onClick={this.props.toggleModal}>&times;</div>
+                </article>
+            </div>
+        )}
+    }
+    
 export default Profile;
